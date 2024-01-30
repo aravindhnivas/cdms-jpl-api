e8a56d8c4bfbc98064c12e0e7a4a29ba16d7ea7c
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-30 12:13:51 -0500

    Update output_parser.ts

--- output_parser.ts
+++ output_parser.ts
@@ -13,6 +13,17 @@ async function main(tag: string = "005502") {
   const references: string[] = [];
 
   // console.log("\n\nscraping references\n\n");
+  const td_val = $("td[align='right']");
+  const td_parent = td_val.parent();
+
+  const full_info = {};
+  for (const arr of td_parent.toArray()) {
+    const key = $(arr?.firstChild).text().trim();
+    const value = $(arr.lastChild).text().trim();
+    full_info[key] = value;
+    // console.log({ key, value });
+  }
+
   for (const element of ref_element.toArray()) {
     let ref = $(element).text();
     ref = ref.replaceAll(/(\(\d\))/g, "").trim();
@@ -20,82 +31,6 @@ async function main(tag: string = "005502") {
     references.push(ref);
   }
 
-  const summary =
-    $("p")
-      .html()
-      ?.split("\n")
-      .map((f) => f.trim())
-      .filter((f) => f) ?? [];
-
-  const summary_points: string[] = [];
-  let init_point = "";
-  let write = true;
-  for (const line of summary) {
-    if (write) init_point += line;
-    if (line.includes("<font")) {
-      // console.log(init_point);
-      summary_points.push(init_point.replace(/<.*?>/g, ""));
-      init_point = "";
-      write = false;
-    }
-    if (!write) {
-      if (line.includes("</font>")) {
-        write = true;
-      }
-    }
-  }
-  // console.log({ summary_points });
-  // console.log(references.length, summary_points.length);
-  // return;
-
-  const qpart = {};
-
-  const keys = [
-    "Lines Listed",
-    "Frequency / GHz",
-    "Max. J",
-    "log STR0",
-    "log STR1",
-    "Isotope Corr.",
-    "Egy / (cm-1)",
-    "µa / D",
-    "µb / D",
-    "µc / D",
-    "A / MHz",
-    "B / MHz",
-    "C / MHz",
-    "detected in ISM/CSM",
-  ];
-
-  const metakeys = ["Species tag", "Version", "Date of Entry", "Contributor"];
-  const values = {};
-  const meta = {};
-
-  const str = $("td").text();
-  const splitLines = str.split("\n");
-
-  for (let line of splitLines.slice(0, splitLines.length - 1)) {
-    line = line.trim();
-    for (const key of keys) {
-      if (line.includes(key) && !values[key]) {
-        const [, splitLine] = line.split(key);
-        values[key] = splitLine.trim();
-      }
-    }
-
-    for (const mkey of metakeys) {
-      if (line.includes(mkey) && !meta[mkey]) {
-        const [, splitLine] = line.split(mkey);
-        meta[mkey] = splitLine.trim();
-      }
-    }
-
-    if (line.startsWith("Q(") && line.match(/Q\((\d+(\.\d*)?)\)/g)) {
-      const [key, ...value] = line.split(")");
-      const joined_val = value.join("");
-      qpart[key.replaceAll(/(Q\()/g, "")] = joined_val + (joined_val.includes("(") ? ")" : "");
-    }
-  }
   const heading = $("caption font:not([color='red'])");
   // console.log(heading.html());
 
@@ -109,7 +44,11 @@ async function main(tag: string = "005502") {
       ?.split("\n")[1]
       .split(/[,;]/g)
       .map((f) => f.trim()) ?? [];
-  await Bun.write("./parsed.json", JSON.stringify({ name: { default: iupac_name, meta: name_meta, formula: name_formula, html: name_html }, qpart, values, meta, references }, null, 2));
+  const name = { default: iupac_name, meta: name_meta, formula: name_formula, html: name_html };
+  const processed_informations = { name, ...full_info, references };
+
+  await Bun.write("./full_info.json", JSON.stringify(processed_informations, null, 2));
+  return processed_informations;
 }
 
-main("005501");
+main("005502");
