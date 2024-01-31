8f8cc3644962d26efc480647f5ec7d5667a9d7b0
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-31 12:43:33 -0500

    prettier format

--- parse_by_tagname.ts
+++ parse_by_tagname.ts
@@ -1,81 +1,81 @@
-import * as Bun from "bun";
-import axios from "axios";
-import * as cheerio from "cheerio";
+import * as Bun from 'bun';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 function endash_str(str: string) {
-  return str.replaceAll("–", "-").trim();
+	return str.replaceAll('–', '-').trim();
 }
 
-async function main(tag: string = "005502") {
-  tag = tag.padStart(6, "0");
-  const entries_url = `https://cdms.astro.uni-koeln.de/cgi-bin/cdmsinfo?file=e${tag}.cat`;
+async function main(tag: string = '005502') {
+	tag = tag.padStart(6, '0');
+	const entries_url = `https://cdms.astro.uni-koeln.de/cgi-bin/cdmsinfo?file=e${tag}.cat`;
 
-  const { data } = await axios.get(entries_url);
-  const $ = cheerio.load(data);
-  const ref_element = $('p font[color="#064898"]');
-  const references: string[] = [];
+	const { data } = await axios.get(entries_url);
+	const $ = cheerio.load(data);
+	const ref_element = $('p font[color="#064898"]');
+	const references: string[] = [];
 
-  // console.log("\n\nscraping references\n\n");
-  const td_val = $("td[align='right']");
-  const td_parent = td_val.parent();
+	// console.log("\n\nscraping references\n\n");
+	const td_val = $("td[align='right']");
+	const td_parent = td_val.parent();
 
-  const full_info = {};
-  for (const arr of td_parent.toArray()) {
-    const key = $(arr?.firstChild).text().trim();
-    // const value = $(arr.lastChild).text().trim();
-    let value: string | string[] = "";
+	const full_info = {};
+	for (const arr of td_parent.toArray()) {
+		const key = $(arr?.firstChild).text().trim();
+		// const value = $(arr.lastChild).text().trim();
+		let value: string | string[] = '';
 
-    if (key === "Contributor") {
-      if ($(arr.lastChild).html()?.includes("<br>")) {
-        value = $(arr.lastChild).html()?.split("<br>");
-      } else {
-        value = [endash_str($(arr.lastChild).text().trim())];
-      }
-    } else {
-      value = endash_str($(arr.lastChild).text().trim());
-    }
-    full_info[endash_str(key)] = value;
-  }
+		if (key === 'Contributor') {
+			if ($(arr.lastChild).html()?.includes('<br>')) {
+				value = $(arr.lastChild).html()?.split('<br>');
+			} else {
+				value = [endash_str($(arr.lastChild).text().trim())];
+			}
+		} else {
+			value = endash_str($(arr.lastChild).text().trim());
+		}
+		full_info[endash_str(key)] = value;
+	}
 
-  for (const element of ref_element.toArray()) {
-    let ref = $(element).text();
-    ref = ref.replaceAll(/(\(\d\))/g, "").trim();
-    ref = ref.replaceAll("\n", " ").replaceAll("  ", " ");
-    references.push(endash_str(ref));
-  }
+	for (const element of ref_element.toArray()) {
+		let ref = $(element).text();
+		ref = ref.replaceAll(/(\(\d\))/g, '').trim();
+		ref = ref.replaceAll('\n', ' ').replaceAll('  ', ' ');
+		references.push(endash_str(ref));
+	}
 
-  const heading = $("caption font:not([color='red'])");
-  console.log(heading.html()?.split(/[, ]/g));
-  const [name_formula, ...name_formula_meta] = heading.text()?.trim()?.split(/[, ]/g);
-  const [name_html, ...name_html_meta] =
-    heading
-      .html()
-      ?.replace(/<\/?font.*?>/g, "")
-      .split(/[, ]/g) ?? "";
+	const heading = $("caption font:not([color='red'])");
+	console.log(heading.html()?.split(/[, ]/g));
+	const [name_formula, ...name_formula_meta] = heading.text()?.trim()?.split(/[, ]/g);
+	const [name_html, ...name_html_meta] =
+		heading
+			.html()
+			?.replace(/<\/?font.*?>/g, '')
+			.split(/[, ]/g) ?? '';
 
-  const iupac_name = $("caption").text().split("\n")[1].split(/[,;]/g)[0];
-  const [, ...name_meta] =
-    $("caption")
-      .html()
-      ?.split("\n")[1]
-      .split(/[,;]/g)
-      .map((f) => endash_str(f.trim())) ?? [];
+	const iupac_name = $('caption').text().split('\n')[1].split(/[,;]/g)[0];
+	const [, ...name_meta] =
+		$('caption')
+			.html()
+			?.split('\n')[1]
+			.split(/[,;]/g)
+			.map((f) => endash_str(f.trim())) ?? [];
 
-  const name = {
-    default: endash_str(iupac_name),
-    meta: name_meta.join(", "),
-    formula: {
-      default: endash_str(name_formula),
-      meta: endash_str(name_formula_meta.join(" ").replaceAll("  ", ", ")),
-    },
-    html: {
-      default: endash_str(name_html),
-      meta: endash_str(name_html_meta.join(" ").replaceAll("  ", ", ")),
-    },
-  };
-  const processed_informations = { name, ...full_info, references };
-  await Bun.write("./full_info.json", JSON.stringify(processed_informations, null, 2));
-  return processed_informations;
+	const name = {
+		default: endash_str(iupac_name),
+		meta: name_meta.join(', '),
+		formula: {
+			default: endash_str(name_formula),
+			meta: endash_str(name_formula_meta.join(' ').replaceAll('  ', ', '))
+		},
+		html: {
+			default: endash_str(name_html),
+			meta: endash_str(name_html_meta.join(' ').replaceAll('  ', ', '))
+		}
+	};
+	const processed_informations = { name, ...full_info, references };
+	await Bun.write('./full_info.json', JSON.stringify(processed_informations, null, 2));
+	return processed_informations;
 }
 
-main("039502");
+main('039502');
--- parse_full_table.ts
+++ parse_full_table.ts
@@ -1,69 +1,82 @@
-import axios from "axios";
-import * as cheerio from "cheerio";
-import * as Bun from "bun";
-import * as json2csv from "json2csv";
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import * as Bun from 'bun';
+import * as json2csv from 'json2csv';
 
-const CDMS_URL = "https://cdms.astro.uni-koeln.de";
+const CDMS_URL = 'https://cdms.astro.uni-koeln.de';
 const fetch_cdms_url = async () => {
-  console.log(`fetching data from ${CDMS_URL}`);
-  const res = await axios.get(CDMS_URL + "/classic/entries");
-  if (res.status !== 200) throw new Error("Failed to fetch data");
-  return res.data;
+	console.log(`fetching data from ${CDMS_URL}`);
+	const res = await axios.get(CDMS_URL + '/classic/entries');
+	if (res.status !== 200) throw new Error('Failed to fetch data');
+	return res.data;
 };
 
 const write_json_file = async (data: any, filename: string) => {
-  console.log("writing json");
-  await Bun.write(`./temp/${filename}.json`, JSON.stringify(data, null, 2));
-  console.log(`wrote to ${filename}.json`);
+	console.log('writing json');
+	await Bun.write(`./temp/${filename}.json`, JSON.stringify(data, null, 2));
+	console.log(`wrote to ${filename}.json`);
 };
 
-export async function get_cdms_data(data: string, filename: string = "cdms_data", write_json: boolean = true) {
-  if (!data) throw new Error("No data provided");
-  const $ = cheerio.load(data);
+export async function get_cdms_data(
+	data: string,
+	filename: string = 'cdms_data',
+	write_json: boolean = true
+) {
+	if (!data) throw new Error('No data provided');
+	const $ = cheerio.load(data);
 
-  let tableData = [];
-  console.log("parsing table");
+	let tableData = [];
+	console.log('parsing table');
 
-  const columns: string[] = [];
-  $("th").each((index, column) => {
-    const header_name = $(column).text().trim().replace("–", "-");
-    if (header_name) columns.push(header_name);
-  });
+	const columns: string[] = [];
+	$('th').each((index, column) => {
+		const header_name = $(column).text().trim().replace('–', '-');
+		if (header_name) columns.push(header_name);
+	});
 
-  $("table")
-    .find("tr")
-    .each((index, row) => {
-      let cols: Record<string, string> = {};
-      $(row)
-        .find("td")
-        .each((i, col) => {
-          if (!(columns[i] === "Catalog" || columns[i] === "Entry in cm-1" || columns[i] === "Documentation")) {
-            const val = $(col).text().trim().replace("–", "-");
-            cols[columns[i]] = val;
-          }
-        });
-      tableData.push(cols);
-    });
-  console.log("finished fetching data");
-  if (write_json) {
-    await write_json_file(tableData, filename);
-  }
-  return tableData;
+	$('table')
+		.find('tr')
+		.each((index, row) => {
+			let cols: Record<string, string> = {};
+			$(row)
+				.find('td')
+				.each((i, col) => {
+					if (
+						!(
+							columns[i] === 'Catalog' ||
+							columns[i] === 'Entry in cm-1' ||
+							columns[i] === 'Documentation'
+						)
+					) {
+						const val = $(col).text().trim().replace('–', '-');
+						cols[columns[i]] = val;
+					}
+				});
+			tableData.push(cols);
+		});
+	console.log('finished fetching data');
+	if (write_json) {
+		await write_json_file(tableData, filename);
+	}
+	return tableData;
 }
 
-export async function html_to_csv(data: string[], filename: string = "output.csv") {
-  console.log("converting to csv");
-  const csv = json2csv.parse(data);
-  console.log("writing csv");
-  await Bun.write(`./temp/${filename}.csv`, csv);
-  console.log(`wrote to ${filename}.csv`);
+export async function html_to_csv(data: string[], filename: string = 'output.csv') {
+	console.log('converting to csv');
+	const csv = json2csv.parse(data);
+	console.log('writing csv');
+	await Bun.write(`./temp/${filename}.csv`, csv);
+	console.log(`wrote to ${filename}.csv`);
 }
 
-try {
-  const cdms_html = await fetch_cdms_url();
-  console.log("fetched data");
-  const cdms_data = await get_cdms_data(cdms_html);
-  html_to_csv(cdms_data, "cdms_data");
-} catch (error) {
-  console.warn(error);
-}
+const fetch_cdms = async () => {
+	try {
+		const cdms_html = await fetch_cdms_url();
+		console.log('fetched data');
+
+		const cdms_data = await get_cdms_data(cdms_html);
+		html_to_csv(cdms_data, 'cdms_data');
+	} catch (error) {
+		console.warn(error);
+	}
+};
