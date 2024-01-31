408b65689e35fe193587d3e72fb0bac04a0bbed4
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-31 13:07:48 -0500

    Update parse_full_table.ts

--- parse_full_table.ts
+++ parse_full_table.ts
@@ -11,19 +11,55 @@ const fetch_cdms_url = async () => {
 	return res.data;
 };
 
+const JPL_URL = 'https://spec.jpl.nasa.gov';
+const fetch_jpl_url = async () => {
+	console.log(`fetching data from ${JPL_URL}`);
+	const res = await axios.get(JPL_URL + '/ftp/pub/catalog/catdir.html');
+	if (res.status !== 200) throw new Error('Failed to fetch data');
+	return res.data;
+};
+
 const write_json_file = async (data: any, filename: string) => {
 	console.log('writing json');
 	await Bun.write(`./temp/${filename}.json`, JSON.stringify(data, null, 2));
 	console.log(`wrote to ${filename}.json`);
 };
 
+const write_txt_file = async (data: any, filename: string) => {
+	console.log('writing txt');
+	await Bun.write(`./temp/${filename}.txt`, data);
+	console.log(`wrote to ${filename}.txt`);
+};
+
+export async function get_jpl_data(jpl_html_string: string, filename: string = 'jpl_data') {
+	if (!jpl_html_string) throw new Error('No data provided');
+	const $ = cheerio.load(jpl_html_string);
+	const jpl_data = $('pre').text();
+
+	const jpl_data_arr = jpl_data.split('\n').map((line) => line.trim());
+	const columns = ['ID', 'Name', '# lines', 'Version'];
+	const data = jpl_data_arr.slice(1);
+	const jpl_data_obj = data.map((row) => {
+		const mod_row = row.split(/\s+/).slice(0, 4);
+		let obj: Record<string, string> = {};
+		columns.forEach((column, index) => {
+			obj[column] = mod_row[index];
+		});
+		return obj;
+	});
+	await write_json_file(jpl_data_obj, 'jpl_data');
+
+	console.log('finished fetching JPL data');
+	return jpl_data_obj;
+}
+
 export async function get_cdms_data(
-	data: string,
+	cdms_html_string: string,
 	filename: string = 'cdms_data',
 	write_json: boolean = true
 ) {
-	if (!data) throw new Error('No data provided');
-	const $ = cheerio.load(data);
+	if (!cdms_html_string) throw new Error('No data provided');
+	const $ = cheerio.load(cdms_html_string);
 
 	let tableData = [];
 	console.log('parsing table');
@@ -61,7 +97,7 @@ export async function get_cdms_data(
 	return tableData;
 }
 
-export async function html_to_csv(data: string[], filename: string = 'output.csv') {
+export async function html_to_csv(data: string[] | Object, filename: string = 'output.csv') {
 	console.log('converting to csv');
 	const csv = json2csv.parse(data);
 	console.log('writing csv');
@@ -80,3 +116,17 @@ const fetch_cdms = async () => {
 		console.warn(error);
 	}
 };
+
+const fetch_jpl = async () => {
+	try {
+		const jpl_html = await fetch_jpl_url();
+		console.log('fetched data');
+
+		const jpl_data_obj = await get_jpl_data(jpl_html);
+		html_to_csv(jpl_data_obj, 'jpl_data');
+	} catch (error) {
+		console.warn(error);
+	}
+};
+
+await fetch_jpl();
