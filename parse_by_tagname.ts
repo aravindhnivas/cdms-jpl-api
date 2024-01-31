a95d7caf31eaba9dc3930f7e24590ee1181d55b6
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-31 13:59:53 -0500

    Update parse_by_tagname.ts

--- parse_by_tagname.ts
+++ parse_by_tagname.ts
@@ -6,11 +6,11 @@ function endash_str(str: string) {
 	return str.replaceAll('–', '-').trim();
 }
 
-async function main(tag: string = '005502') {
+export async function CDMS(tag: string = '005502') {
 	tag = tag.padStart(6, '0');
 	const entries_url = `https://cdms.astro.uni-koeln.de/cgi-bin/cdmsinfo?file=e${tag}.cat`;
-
 	const { data } = await axios.get(entries_url);
+
 	const $ = cheerio.load(data);
 	const ref_element = $('p font[color="#064898"]');
 	const references: string[] = [];
@@ -45,7 +45,7 @@ async function main(tag: string = '005502') {
 	}
 
 	const heading = $("caption font:not([color='red'])");
-	console.log(heading.html()?.split(/[, ]/g));
+	// console.log(heading.html()?.split(/[, ]/g));
 	const [name_formula, ...name_formula_meta] = heading.text()?.trim()?.split(/[, ]/g);
 	const [name_html, ...name_html_meta] =
 		heading
@@ -74,8 +74,66 @@ async function main(tag: string = '005502') {
 		}
 	};
 	const processed_informations = { name, ...full_info, references };
-	await Bun.write('./full_info.json', JSON.stringify(processed_informations, null, 2));
+	await Bun.write(`./temp/cdms_${tag}_data.json`, JSON.stringify(processed_informations, null, 2));
 	return processed_informations;
 }
 
-main('039502');
+// CDMS('039502');
+
+export async function JPL(tag: string = '1001') {
+	tag = tag.padStart(6, '0');
+
+	const entries_url = `https://spec.jpl.nasa.gov/ftp/pub/catalog/doc/d${tag}.cat`;
+	const { data } = await axios.get(entries_url);
+
+	const datalines = data.split('\n');
+
+	const data_obj = {};
+	const qpart = {};
+	const rot_const = {};
+
+	for (let line of datalines) {
+		line = line.replaceAll(/[\\\\\$\^\{\}:=]/g, '').trim();
+
+		if (line.includes('headend')) break;
+		if (!line) continue;
+		console.log(line);
+		if (line.match(/Q\(\d+\.\d+?\)/g)) {
+			const [key, value] = line
+				.split('&')
+				.map((f) => f.trim())
+				.slice(2, 4);
+			qpart[key] = value;
+		}
+
+		if (line.match(/[ABC]&/g)) {
+			console.log(line);
+			const [key, value] = line
+				.split('&')
+				.map((f) => f.trim())
+				.slice(2, 4);
+			rot_const[key] = value;
+		}
+		let [key, value] = line
+			.split('&')
+			.map((f) => f.trim())
+			.slice(0, 2);
+
+		if (key.match(/[><]/g)) {
+			// Split the key on the space character
+			const parts = key.split(' ');
+			// Reassemble the key and value
+			key = parts.slice(0, -1).join(' ');
+			value = parts[parts.length - 1] + ' ' + value;
+		}
+
+		if (key) data_obj[key] = value;
+	}
+	await Bun.write(
+		`./temp/jpl_${tag}_data.json`,
+		JSON.stringify({ ...data_obj, qpart, rot_const }, null, 2)
+	);
+	// const columns = ["Species Tag", "Version", "Date", "Contributor", "Lines Listed", "Freq. (GHz) <", "Max. J", "LOGSTR0", "LOGSTR1", "Isotope Corr.", "Egy. (cm$^{-1}$) $>$", "$\\mu_a$ =", "$\\mu_b$ =", "$\\mu_c$ ="];
+}
+// CDMS('004501');
+JPL('4001');
