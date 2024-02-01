7ee78fee4a39b42b3527a6cac75c34d5303f68a3
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-02-01 12:02:57 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export { CDMS, JPL } from './parse_by_tagname';
 export { parse_cdms_data, parse_jpl_data } from './parse_full_table';
 export {
-	fetch_data_from_url,
+	URLs,
 	html_to_csv,
-	url_from_cdms_tag,
-	url_from_jpl_tag,
-	write_json_file,
 	write_txt_file,
-	URLs
+	write_json_file,
+	url_from_jpl_tag,
+	url_from_cdms_tag,
+	fetch_data_from_url
 } from './utils';
