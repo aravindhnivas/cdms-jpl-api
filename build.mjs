a19b77c0d50309ef6f49c6f08b074990bdaba415
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-02-01 12:50:23 -0500

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -1,8 +1,16 @@
 import dts from 'bun-plugin-dts'
 
 await Bun.build({
-  entrypoints: ['./src/index.ts'],
+  entrypoints: [
+    './src/index.ts',
+    // './src/parse_by_tagname.ts', 
+    // './src/parse_full_table.ts', 
+  ],
   outdir: './dist',
   minify: true,
-  plugins: [dts()]
+  target: 'node',
+  splitting: true,
+  plugins: [dts()],
+  format: "esm",
+  sourcemap: 'external'
 })
