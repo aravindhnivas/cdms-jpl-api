5e460ac56fb8e41552ab77848580f5237a5d5c00
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-03-07 11:46:04 -0500

    demo scripts

---
+++ demo/demo_fetch_full_table.ts
@@ -0,0 +1,32 @@
+// import { parse_cdms_data, parse_jpl_data } from 'cdms-jpl-api';
+import { parse_cdms_data, parse_jpl_data } from '../src/index';
+import { html_to_csv, write_json_file, fetch_data_from_url } from './utils';
+
+const fetch_cdms = async () => {
+	try {
+		const cdms_html = await fetch_data_from_url('CDMS');
+		console.log('fetched data');
+
+		const cdms_data = await parse_cdms_data(cdms_html);
+		await write_json_file(cdms_data, 'cdms_data');
+		html_to_csv(cdms_data, 'cdms_data');
+	} catch (error) {
+		console.warn(error);
+	}
+};
+
+await fetch_cdms();
+
+const fetch_jpl = async () => {
+	try {
+		const jpl_html = await fetch_data_from_url('JPL');
+		console.log('fetched data');
+
+		const jpl_data_obj = await parse_jpl_data(jpl_html);
+		await write_json_file(jpl_data_obj, 'jpl_data');
+		html_to_csv(jpl_data_obj, 'jpl_data');
+	} catch (error) {
+		console.warn(error);
+	}
+};
+await fetch_jpl();
---
+++ demo/utils.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import * as Bun from 'bun';
+import * as json2csv from 'json2csv';
+
+export const write_json_file = async (data: any, filename: string) => {
+	console.log('writing json');
+	await Bun.write(`${filename}.json`, JSON.stringify(data, null, 2));
+	console.log(`wrote to ${filename}.json`);
+};
+
+export const write_txt_file = async (data: any, filename: string) => {
+	console.log('writing txt');
+	await Bun.write(`${filename}.txt`, data);
+	console.log(`wrote to ${filename}.txt`);
+};
+
+export const html_to_csv = async (
+	data: (JPLData | CDMSData)[],
+	filename: string = 'output.csv'
+) => {
+	console.log('converting to csv');
+	const csv = json2csv.parse(data);
+	console.log('writing csv');
+	await Bun.write(`${filename}.csv`, csv);
+	console.log(`wrote to ${filename}.csv`);
+};
+
+export const url_from_jpl_tag = (tag: string) => {
+	const tag_num = tag.padStart(6, '0');
+	return `https://spec.jpl.nasa.gov/ftp/pub/catalog/doc/d${tag_num}.cat`;
+};
+
+export const url_from_cdms_tag = (tag: string) => {
+	const tag_num = tag.padStart(6, '0');
+	return `https://cdms.astro.uni-koeln.de/cgi-bin/cdmsinfo?file=e${tag_num}.cat`;
+};
+
+export const URLs_by_tag = {
+	CDMS: url_from_cdms_tag,
+	JPL: url_from_jpl_tag
+};
+
+export const URLs = {
+	CDMS: 'https://cdms.astro.uni-koeln.de/classic/entries',
+	JPL: 'https://spec.jpl.nasa.gov/ftp/pub/catalog/catdir.html'
+};
+
+export const fetch_data_from_url = async (database: 'CDMS' | 'JPL' = 'CDMS') => {
+	const url = URLs[database];
+	console.log(`fetching data from ${url}`);
+	const res = await axios.get(url);
+	if (res.status !== 200) throw new Error('Failed to fetch data');
+	return res.data;
+};
